7a5dd008fc49b5320942547ae40c9b26bfbcc2ca
Luc Engelen <luc@kabisa.nl>
2018-02-06 10:06:51 +0100

    Update bundle

--- www/bundle.js
+++ www/bundle.js
@@ -1,9 +1,28 @@
 'use strict';
 
-Object.defineProperty(exports, '__esModule', { value: true });
+var SERVICE = "IndoorPositioningService";
 
-var message = "Hello world!";
+var forward = function (exec, action) { return function (success, error) { return exec(success, error, SERVICE, action, []); }; };
 
-var alertMessage = function () { return alert(message); };
+var setConfiguration = function (exec) { return function (configuration, success, error) {
+  exec(success, error, SERVICE, "setConfiguration", [configuration]);
+}; };
 
-exports.alertMessage = alertMessage;
+var indoorPositioningService = function () {
+  if (window.cordova) {
+    var exec = window.cordova.exec;
+    return {
+      start: forward(exec, "start"),
+      stop: forward(exec, "stop"),
+      getError: forward(exec, "getError"),
+      getHeading: forward(exec, "getHeading"),
+      getLocation: forward(exec, "getLocation"),
+      setConfiguration: setConfiguration(exec)
+    };
+  } else {
+    // eslint-disable-next-line no-console
+    console.log("Cordova is unavailable");
+  }
+};
+
+window.indoorPositioning = indoorPositioningService();
