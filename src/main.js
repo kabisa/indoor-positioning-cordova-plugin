1c9e9dd4231de383781e794bac0f648a06b50e6b
Luc Engelen <luc@kabisa.nl>
2018-02-06 10:05:29 +0100

    Rename plugin

--- src/main.js
+++ src/main.js
@@ -1,3 +1,3 @@
 import { indoorPositioningService } from "./indoorPositioningService";
 
-window.indoorPositioningService = indoorPositioningService();
+window.indoorPositioning = indoorPositioningService();
